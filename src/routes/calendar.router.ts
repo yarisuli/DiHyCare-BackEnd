71ab6be7e7cc7274ff7756432a2b3c6102aab551
Iara Genco <148562678+yarisuli@users.noreply.github.com>
2025-07-14 13:57:03 -0300

    routes calendario organizadas

--- src/routes/calendar.router.ts
+++ src/routes/calendar.router.ts
@@ -3,10 +3,10 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('', calendarController.getEvents);
-router.get('/:id', calendarController.getEventById);
-router.get('/user', calendarController.getEventByUser);
-router.get('/type', calendarController.getEventByType);
+router.get('/user', calendarController.getEventByUser); 
+router.get('/type', calendarController.getEventByType);       
+router.get('/:id', calendarController.getEventById);          
+router.get('', calendarController.getEvents);                 
 router.post('', calendarController.createEvent);
 router.put('/:id', calendarController.updateEvent);
 router.delete('/:id', calendarController.deleteEvent);
