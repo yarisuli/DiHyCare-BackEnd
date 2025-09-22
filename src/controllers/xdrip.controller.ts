a903a7b2f82a087a5c40fa7187a41fdf3e3a3bd2
Iara Genco <148562678+yarisuli@users.noreply.github.com>
2025-09-22 13:21:18 -0300

    comment xdrip controller

--- src/controllers/xdrip.controller.ts
+++ src/controllers/xdrip.controller.ts
@@ -3,26 +3,26 @@ import xdripService from '../services/xdrip.service';
 import { Request, Response } from 'express';
 import userService from '../services/user.service';
 
-const createEntries = async (req: Request, res: Response) => {
-  try {
-    const entries = req.body as EntryUpload[];
-    if (!entries || entries.length === 0) {
-      return res.status(400).json({ error: 'Could not get entries.' });
-    }
-    const token = '3f509aca-4521-42f7-b621-7ad59db58cd1';
-    const user = await userService.getUserByToken(token);
-    if (!user) {
-      return res.status(404).json({ error: 'User not found.' });
-    }
-    await xdripService.createEntries(user.id, entries);
-    res.status(200).send('Data saved successfully.');
-  } catch (error: any) {
-    return res.status(500).json({ error: error.message });
-  }
-};
+// const createEntries = async (req: Request, res: Response) => {
+//   try {
+//     const entries = req.body as EntryUpload[];
+//     if (!entries || entries.length === 0) {
+//       return res.status(400).json({ error: 'Could not get entries.' });
+//     }
+//     const token = '3f509aca-4521-42f7-b621-7ad59db58cd1';
+//     const user = await userService.getUserByToken(token);
+//     if (!user) {
+//       return res.status(404).json({ error: 'User not found.' });
+//     }
+//     await xdripService.createEntries(user.id, entries);
+//     res.status(200).send('Data saved successfully.');
+//   } catch (error: any) {
+//     return res.status(500).json({ error: error.message });
+//   }
+// };
 
-const xdrip = {
-  createEntries
-};
+// const xdrip = {
+//   createEntries
+// };
 
-export default xdrip;
+// export default xdrip;
