fdf4f8d80facaaf65b8c1f7e6f8eb4b86886f01c
Iara Genco <148562678+yarisuli@users.noreply.github.com>
2025-09-22 19:43:07 -0300

    prettier!!

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -14,17 +14,17 @@ const register = async (email: string, password: string): Promise<User> => {
 };
 
 const login = async (email: string, password: string): Promise<string> => {
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) {
-      throw new Error('User not found');
-    }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      throw new Error('Invalid password');
-    }
-    const token = jwt.sign({ userId: user.id }, 'secret', { expiresIn: '1h' });
-    return token;
-  };
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    throw new Error('User not found');
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error('Invalid password');
+  }
+  const token = jwt.sign({ userId: user.id }, 'secret', { expiresIn: '1h' });
+  return token;
+};
 
 export default {
   register,
